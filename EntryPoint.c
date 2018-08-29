83c79b0b7a7fd59e811d7ec8cd4160f3b04e3275
Vinayak96k <vinayak.patil0304@gmail.com>
2018-08-29 15:03:37 +0530

    first commit

---
+++ EntryPoint.c
@@ -0,0 +1,8 @@
+#include"Myheader.h"
+
+int main(int argc, char const *argv[])
+{
+    display();
+
+    return 0;
+}
---
+++ Helper.c
@@ -0,0 +1,16 @@
+#include"Myheader.h"
+
+/////////////////////////////////////////////////////////////////////////////////////
+//  Function Name: Display
+// Description: Print 5 times marvellous on screen
+//  Author: Vinayak Maherndra Patil
+////////////////////////////////////////////////////////////////////////////////////
+
+void display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Marvellous\t");
+    }
+}
